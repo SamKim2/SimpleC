2af7b706dfc5e7c7231c42e91a0306e1c9bc5a08
Sam, Kim <sam@gmail.com>
2013-08-18 21:16:40 +0900

    My first commit

---
+++ SimpleC.cpp
@@ -0,0 +1,16 @@
+// SimpleC.cpp : 콘솔 응용 프로그램에 대한 진입점을 정의합니다.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout << "Hello, Git!" << "\n";
+
+	return 0;
+}
+
