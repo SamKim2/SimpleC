5dad11964f5204674bbe628e6734cc1e236cc1f4
Sam, Kim <sam@gmail.com>
2013-08-18 22:43:53 +0900

    My second commit.

--- SimpleC.cpp
+++ SimpleC.cpp
@@ -11,6 +11,10 @@ int _tmain(int argc, _TCHAR* argv[])
 {
 	cout << "Hello, Git!" << "\n";
 
+	cout << "Staging modified files" << "\n";
+
+	cout << "Modified SimpleC.cpp again" << "\n";
+
 	return 0;
 }
 
